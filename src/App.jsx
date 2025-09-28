7b8f1d06d18f99b0fc6342106e20a892ba8efdaf
Juliur9 <url.julian@gmail.com>
2025-09-28 11:38:25 +0200

    Login/Register site + Structure of Login and the messenger view

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useState, useEffect } from "react";
+import LoginRegister from "./components/LoginRegister.jsx";
 import Messenger from "./components/Messenger.jsx";
 
 function App() {
-  return <Messenger />;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  return user ? <Messenger user={user} /> : <LoginRegister onLoginSuccess={setUser} />;
 }
 
 export default App;
