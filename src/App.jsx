0033da1c0236d8cb89c929d4179d1f1f19758252
Juliur9 <url.julian@gmail.com>
2025-10-26 12:00:46 +0100

    New routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Messenger from './components/Messenger';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Messenger from "./components/Messenger";
 
 class App extends Component {
-  state = {  } 
-  render() { 
+  state = {};
+  render() {
     return (
       <Router>
-      <div className='routes'>
-        <Routes>
-          <Route path="/" element={<Messenger />} />
-          <Route path="/Login" element={<Login />} />
-        </Routes>
+        <div className="routes">
+          <Routes>
+            <Route path="/messenger" element={<Messenger />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Messenger />} /> //nur temporär
+          </Routes>
         </div>
       </Router>
     );
   }
 }
- 
+
 export default App;
