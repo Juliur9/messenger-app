af9bfcd10fe4b6a845ad00dd3c6627914bbed245
Juliur9 <url.julian@gmail.com>
2025-10-26 15:12:33 +0100

    Update design in Login and Register

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -26,23 +26,19 @@ const Login = () => {
   };
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center" class="login-container">
+    <div className="flex h-screen w-full flex-col items-center justify-center">
       <h1 className="mb-8 text-3xl">Login</h1>
 
-      <form className="w-3xl flex flex-col" onSubmit={handlelogin}>
+      <form className="flex w-80 flex-col" onSubmit={handlelogin}>
         <p>{status}</p>
 
-        <label className="py-1" htmlFor="email">
-          E-Mail:
-        </label>
-        <input className="m-1 mb-5 h-16 w-full rounded-2xl bg-slate-100 p-2" id="email" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <label htmlFor="email">E-Mail:</label>
+        <input className="m-1 mb-5 h-14 w-full rounded-2xl bg-slate-100 p-2" id="email" type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
 
-        <label className="p-1" htmlFor="password">
-          Passwort:{" "}
-        </label>
-        <input className="m-1 mb-5 h-16 w-full rounded-2xl bg-slate-100 p-2" id="password" type="password" placeholder="Passwort" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <label htmlFor="password">Passwort: </label>
+        <input className="m-1 mb-5 h-14 w-full rounded-2xl bg-slate-100 p-2" id="password" type="password" placeholder="Passwort" value={password} onChange={(e) => setPassword(e.target.value)} />
 
-        <button className="m-1 h-16 w-full rounded-2xl bg-slate-200 p-1" type="submit">
+        <button className="m-1 h-14 w-full rounded-2xl bg-slate-200 p-1" type="submit">
           Login
         </button>
       </form>
