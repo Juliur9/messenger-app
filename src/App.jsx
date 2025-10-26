a8d6bf00ac93a4b8c5b6cd506eb47b5cd51d317c
Timondo <timon@klingel-stade.de>
2025-10-26 11:36:50 +0100

    Home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Messenger from './components/Messenger';
 
 class App extends Component {
   state = {  } 
@@ -9,6 +10,7 @@ class App extends Component {
       <Router>
       <div className='routes'>
         <Routes>
+          <Route path="/" element={<Messenger />} />
           <Route path="/Login" element={<Login />} />
         </Routes>
         </div>
