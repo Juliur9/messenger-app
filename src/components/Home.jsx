74fbec4f55e82adf5695c3a3094743c752affe21
Juliur9 <url.julian@gmail.com>
2025-10-26 15:13:12 +0100

    Small change on the Homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Home extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <h1>Hier kommt die Startseite hin</h1>
-        );
-    }
+  state = {};
+  render() {
+    return (
+      <>
+        <h1>Hier kommt die Startseite hin</h1>
+        <a href="/login">Login</a>
+      </>
+    );
+  }
 }
- 
+
 export default Home;
