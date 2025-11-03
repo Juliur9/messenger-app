b6ddae272bb465ea669c7f33758a3297de6b8f53
Juliur9 <url.julian@gmail.com>
2025-11-03 15:01:15 +0100

    Update the design of the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,10 +5,58 @@ class Home extends Component {
   render() {
     return (
       <>
-        <h1>Hier kommt die Startseite hin</h1>
-        <a href="/login">Login</a>
-        <br />
-        <a href="/register">Registrieren</a>
+        <nav className="flex w-full items-center justify-between bg-red-200 p-5 py-8">
+          <h1 className="text-xl">MessengerApp</h1>
+          <div className="flex gap-10">
+            <a className="text-xl" href="/register">
+              Registrieren
+            </a>
+            <a className="text-xl" href="/login">
+              Login
+            </a>
+          </div>
+        </nav>
+        <div className="m-12 text-center">
+          <p>Der Messenger, der (fast) alle Probleme löst.</p>
+        </div>
+        <div className="grid grid-cols-1 gap-4 p-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
+            <p>Sicher</p>
+          </div>
+          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
+            <p>Einfach</p>
+          </div>
+          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
+            <p>Super gemacht</p>
+          </div>
+          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
+            <p>Durchdacht</p>
+          </div>
+          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
+            <p>Überzeugend</p>
+          </div>
+          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
+            <p>Innovativ</p>
+          </div>
+          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
+            <p>Daten geschützt</p>
+          </div>
+          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
+            <p>Perfekt für jedermann</p>
+          </div>
+          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
+            <p>Lieben die Kunden</p>
+          </div>
+          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
+            <p>Ohne Werbung</p>
+          </div>
+          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
+            <p>Meine Daten sind sicher</p>
+          </div>
+          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
+            <p>Meine erste Wahl</p>
+          </div>
+        </div>
       </>
     );
   }
