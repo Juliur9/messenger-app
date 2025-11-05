6f60fdb006c6ceba1cea362bdcb5fedcc7efd646
Timondo <timon@klingel-stade.de>
2025-11-05 17:06:08 +0100

    Fehler behoben

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -9,7 +9,7 @@ const UserList = () => {
 useEffect(() => {
     const fetchUsers = async () => {
         try{
-            res = await axios.get("https://messengerapp-backend.onrender.com/api/auth/user", { withCredentials: true }) //Das muss noch rein
+           const res = await axios.get("https://messengerapp-backend.onrender.com/api/auth/user", { withCredentials: true }) //Das muss noch rein
             setUsers(res.data);
         } catch (err) {
             setError('Fehler beim Laden der Daten')
