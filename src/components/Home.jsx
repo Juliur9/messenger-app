08fce2c6fef5858fe3e3f6d19234bc76e51a9092
Timon <timon@MacBook-Pro-von-Ron.local>
2026-01-04 11:35:06 +0100

    Design update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,62 +1,23 @@
 import React, { Component } from "react";
+import Snowfall from 'react-snowfall'
 
 class Home extends Component {
   state = {};
   render() {
-    return (
+    return ( 
       <>
-        <nav className="flex w-full items-center justify-between bg-red-200 p-5 py-8">
-          <h1 className="text-xl">MessengerApp</h1>
+        <nav className="flex w-full h-24 items-center justify-between bg-gradient-to-b from-gray-900 via-gray-800 to-gray-800 p-5">
+          <h1 className="text-3xl font-bold bg-gradient-to-r from-green-500 via-orange-500 via-yellow-500 via-green-500 via-blue-500 to-purple-500 bg-clip-text text-transparent">MessengerApp</h1>
           <div className="flex gap-10">
-            <a className="text-xl" href="/register">
+            <a className="text-3xl font-bold bg-gradient-to-r from-green-500 via-orange-500 via-yellow-500 via-green-500 via-blue-500 to-purple-500 bg-clip-text text-transparent" href="/register">
               Registrieren
             </a>
-            <a className="text-xl" href="/login">
+            <a className="text-3xl font-bold bg-gradient-to-r from-green-500 via-orange-500 via-yellow-500 via-green-500 via-blue-500 to-purple-500 bg-clip-text text-transparent" href="/login">
               Login
             </a>
           </div>
         </nav>
-        <div className="m-12 text-center">
-          <p>Der Messenger, der (fast) alle Probleme löst.</p>
-        </div>
-        <div className="grid grid-cols-1 gap-4 p-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
-          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
-            <p>Sicher</p>
-          </div>
-          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
-            <p>Einfach</p>
-          </div>
-          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
-            <p>Super gemacht</p>
-          </div>
-          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
-            <p>Durchdacht</p>
-          </div>
-          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
-            <p>Überzeugend</p>
-          </div>
-          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
-            <p>Innovativ</p>
-          </div>
-          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
-            <p>Daten geschützt</p>
-          </div>
-          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
-            <p>Perfekt für jedermann</p>
-          </div>
-          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
-            <p>Lieben die Kunden</p>
-          </div>
-          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
-            <p>Ohne Werbung</p>
-          </div>
-          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
-            <p>Meine Daten sind sicher</p>
-          </div>
-          <div className="rounded-md bg-slate-100 p-6 transition-shadow duration-300 hover:shadow-lg">
-            <p>Meine erste Wahl</p>
-          </div>
-        </div>
+        <div className="w-full h-screen bg-gradient-to-r from-green-300 via-violet-600 to-cyan-400"></div>
       </>
     );
   }
