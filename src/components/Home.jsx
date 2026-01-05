c4f78f35e5d3c7d4ec5be4a792dbe97c0de963b3
Juliur9 <url.julian@gmail.com>
2026-01-05 10:58:31 +0100

    Line height + function statt component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,28 +2,27 @@ import { Component } from "react";
 import PublicNavbar from "./publicNavbar";
 
 function Home() {
-
   return (
     <>
-    <PublicNavbar />
-    <div className="flex min-h-screen flex-col">
-      <div className="w-full flex-1 bg-gradient-to-r from-green-300 via-violet-600 to-cyan-400">
-        <div className="grid grid-cols-1 gap-3 p-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
-          <Card text="Sicher" />
-          <Card text="Einfach" />
-          <Card text="Super gemacht" />
-          <Card text="Durchdacht" />
-          <Card text="Überzeugend" />
-          <Card text="Innovativ" />
-          <Card text="Daten geschützt" />
-          <Card text="Perfekt für jedermann" />
-          <Card text="Lieben die Kunden" />
-          <Card text="Ohne Werbung" />
-          <Card text="Meine Daten sind sicher" />
-          <Card text="Meine erste Wahl" />
+      <div className="flex min-h-screen flex-col">
+        <PublicNavbar />
+        <div className="w-full flex-1 bg-gradient-to-r from-green-300 via-violet-600 to-cyan-400">
+          <div className="grid grid-cols-1 gap-3 p-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6">
+            <Card text="Sicher" />
+            <Card text="Einfach" />
+            <Card text="Super gemacht" />
+            <Card text="Durchdacht" />
+            <Card text="Überzeugend" />
+            <Card text="Innovativ" />
+            <Card text="Daten geschützt" />
+            <Card text="Perfekt für jedermann" />
+            <Card text="Lieben die Kunden" />
+            <Card text="Ohne Werbung" />
+            <Card text="Meine Daten sind sicher" />
+            <Card text="Meine erste Wahl" />
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
--- src/components/publicNavbar.jsx
+++ src/components/publicNavbar.jsx
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
-
-
-class publicNavbar extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <>
-        <nav className="flex h-24 w-full items-center justify-between bg-gradient-to-b from-gray-900 via-gray-800 to-gray-800 p-5">
-        <h1 className="bg-gradient-to-r from-green-500 via-blue-500 to-purple-500 bg-clip-text text-3xl font-bold text-transparent">MessengerApp</h1>
+function publicNavbar() {
+  return (
+    <>
+      <nav className="flex h-24 w-full items-center justify-between bg-gradient-to-b from-gray-900 via-gray-800 to-gray-800 p-5">
+        <h1 className="bg-gradient-to-r from-green-500 via-blue-500 to-purple-500 bg-clip-text text-3xl font-bold leading-snug text-transparent">MessengerApp</h1>
         <div className="flex gap-10">
-          <a className="bg-gradient-to-r from-green-500 via-blue-500 to-purple-500 bg-clip-text text-3xl font-bold text-transparent" href="/register">
+          <a className="bg-gradient-to-r from-green-500 via-blue-500 to-purple-500 bg-clip-text text-3xl font-bold leading-snug text-transparent" href="/register">
             Registrieren
           </a>
-          <a className="bg-gradient-to-r from-green-500 via-blue-500 to-purple-500 bg-clip-text text-3xl font-bold text-transparent transition-all duration-300" href="/login">
+          <a className="bg-gradient-to-r from-green-500 via-blue-500 to-purple-500 bg-clip-text text-3xl font-bold leading-snug text-transparent transition-all duration-300" href="/login">
             Login
           </a>
         </div>
       </nav>
-      </>
-        );
-    }
+    </>
+  );
 }
- 
+
 export default publicNavbar;
